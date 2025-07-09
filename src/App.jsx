d478486ffaa46648b92bea46198d91c36c38a6b8
Muhammad Zeeshan Ameer <muhammadzeeshanameer92@gmail.com>
2025-07-09 15:09:03 +0500

    cors policy error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { AdminContext } from './context/AdminContext.jsx';
 import Navbar from './component/Navbar';
 import Sidebar from './component/Sidebar';
 import Home from './component/Home.jsx';
-import {Route , Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import AddDoct from './pages/Admin/AddDoct.jsx';
 import AllAppointments from './pages/Admin/AllAppointments.jsx';
 import Dashboard from './pages/Admin/Dashboard.jsx';
@@ -19,36 +19,36 @@ import DocProfile from './pages/Doctor/DocProfile.jsx';
 
 function App() {
 
-  const { aToken } = useContext(AdminContext);  
-  const {dToken} = useContext(DoctorContext);
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
-
-  return aToken || dToken ? (
-    <div className='bg-[#F8F9FD]'>
-      <ToastContainer />
-      <Navbar/>
-      <div className='flex items-start'>
-        <Sidebar/>
-        <Routes>
-          {/* Admin Routes */}
-          <Route path="/" element={ <Home/>} />
-          <Route path="/add-doctor" element={<AddDoct/>} />
-          <Route path="/all-appointment" element={<AllAppointments/>} />
-          <Route path="/admin-dashboard" element={<Dashboard />} />
-          <Route path="/doctor-list" element={<DoctorList />} />
-          {/* Doctor Routes */}
-          <Route path="/doc-dashboard" element={<DocDashBoard />} />
-          <Route path="/doc-profile" element={<DocProfile />} />
-          <Route path="/doc-appointment" element={<DocAppointment />} />
-        </Routes>
+  return aToken || dToken ?
+    (
+      <div className='bg-[#F8F9FD]'>
+        <ToastContainer />
+        <Navbar />
+        <div className='flex items-start'>
+          <Sidebar />
+          <Routes>
+            {/* Admin Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/add-doctor" element={<AddDoct />} />
+            <Route path="/all-appointment" element={<AllAppointments />} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/doctor-list" element={<DoctorList />} />
+            {/* Doctor Routes */}
+            <Route path="/doc-dashboard" element={<DocDashBoard />} />
+            <Route path="/doc-profile" element={<DocProfile />} />
+            <Route path="/doc-appointment" element={<DocAppointment />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  ) : (
+    ) : (
       <>
         <Login />
         <ToastContainer />
       </>
-  )
+    )
 }
 
 export default App;
